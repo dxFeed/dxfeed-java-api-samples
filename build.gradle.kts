35ff17db3da674ad297b047675fd504b4a1a39cf
Aleksei Sheifler <asheifler@devexperts.com>
2024-04-26 13:36:33 +0200

    template gradle-project

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("java")
+}
+
+group = "com.dxfeed"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven(
+        url = "https://dxfeed.jfrog.io/artifactory/maven-open/"
+    )
+}
+
+dependencies {
+    implementation("com.devexperts.qd:qds:3.327")
+}
